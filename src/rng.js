1c452a6755df1396c143b31d27d0f624b9c92e48
Felix Barz <felix.barz@brickmakers.de>
2020-09-30 11:41:42 +0200

    Habs gefixed  ( fixed #1 )

--- src/rng.js
+++ src/rng.js
@@ -6,10 +6,7 @@ const c = 181739644;
 let value = 0;
 
 export const seed = (seed) => {
-  console.log(seed);
-  // seed is a number between 0 and 1
-  // how can I use that as integer seed ?!?
-  // TODO implement later
+  value = (seed * m).toFixed(0) % m;
 }
 
 export const next = () => {
